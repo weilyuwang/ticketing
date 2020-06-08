7c98c51ab381108410fa2918aeddc4d5900b9dcd
weilyuwang <weilyuwang@gmail.com>
2020-06-08 18:35:33 -0400

    ingress-nginx setup

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,10 @@ import { json } from "body-parser";
 const app = express();
 app.use(json());
 
+app.get("/api/users/currentuser", (req, res) => {
+    res.send("Hi there!");
+});
+
 app.listen(3000, () => {
     console.log("Auth Service listening on port 3000");
 });
