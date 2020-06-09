e26872916804ac388ab64735d1a28ed8b1facd55
weilyuwang <weilyuwang@gmail.com>
2020-06-09 13:24:19 -0400

    determine error types

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,4 +1,6 @@
 import { Request, Response, NextFunction } from "express";
+import { RequestValidationError } from "../errors/request-validation-errors";
+import { DatabaseConnectionError } from "../errors/database-connection-error";
 
 export const errorHandler = (
     err: Error,
@@ -6,7 +8,13 @@ export const errorHandler = (
     res: Response,
     next: NextFunction
 ) => {
-    console.log("Something went wrong", err);
+    if (err instanceof RequestValidationError) {
+        console.log("Handling this error as a request validation error");
+    }
+
+    if (err instanceof DatabaseConnectionError) {
+        console.log("Handling this error as a db connection error");
+    }
 
     res.status(400).send({
         message: err.message,
