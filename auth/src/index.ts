c524388ef9fdf7ee7b02d2a6aa4aa19df23e9332
weilyuwang <weilyuwang@gmail.com>
2020-06-09 14:58:08 -0400

    connect to mongodb with mongoose

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import "express-async-errors";
 import { json } from "body-parser";
+import mongoose from "mongoose";
 
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
@@ -19,12 +20,28 @@ app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
 
+// use express-async-errors lib behind the scene to handle async errors
 app.all("*", async (req, res) => {
     throw new NotFoundError();
 });
 
 app.use(errorHandler);
 
-app.listen(3000, () => {
-    console.log("Auth Service listening on port 3000");
-});
+const start = async () => {
+    try {
+        await mongoose.connect("mongodb://auth-mongo-srv:27017/auth", {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+        });
+        console.log("Connected to MongoDB");
+    } catch (err) {
+        console.log(err);
+    }
+
+    app.listen(3000, () => {
+        console.log("Auth Service listening on port 3000");
+    });
+};
+
+start();
