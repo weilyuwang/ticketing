880a376c1abe237038520606bbe688c8a9e8e277
weilyuwang <weilyuwang@gmail.com>
2020-06-09 21:50:28 -0400

    implement hash method

---
+++ auth/src/services/password.ts
@@ -0,0 +1,15 @@
+import { scrypt, randomBytes } from "crypto";
+import { promisify } from "util";
+
+const scryptAsync = promisify(scrypt);
+
+export class Password {
+    static async toHash(password: string) {
+        const salt = randomBytes(8).toString("hex");
+        const buf = (await scryptAsync(password, salt, 64)) as Buffer;
+
+        return `${buf.toString("hex")}.${salt}`;
+    }
+
+    static compare(storedPassword: string, suppliedPassword: string) {}
+}
