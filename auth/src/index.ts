24b1b7f7999863999b90ca359409454798868164
weilyuwang <weilyuwang@gmail.com>
2020-06-10 14:22:59 -0400

    use cookie-session lib

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import "express-async-errors";
 import { json } from "body-parser";
 import mongoose from "mongoose";
+import cookieSession from "cookie-session";
 
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
@@ -13,8 +14,16 @@ import { errorHandler } from "./middlewares/error-handler";
 import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
+app.set("trust proxy", true); // trust ingress & nginx proxy
 app.use(json());
+app.use(
+    cookieSession({
+        signed: false, // disable encryption on the cookie - JWT is already secured
+        secure: true, // HTTPS connection only
+    })
+);
 
+// express routes
 app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
