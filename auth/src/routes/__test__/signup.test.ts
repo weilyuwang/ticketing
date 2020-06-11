778c61874487961e7a06f782961ae17fd1a6c996
weilyuwang <weilyuwang@gmail.com>
2020-06-10 22:59:40 -0400

    add invalid input test

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -1,7 +1,7 @@
 import request from "supertest";
 import { app } from "../../app";
 
-it("returns a status code 201 on successful signup", async () => {
+it("returns a 201 on successful signup", async () => {
     return request(app)
         .post("/api/users/signup")
         .send({
@@ -9,4 +9,24 @@ it("returns a status code 201 on successful signup", async () => {
             password: "test_password",
         })
         .expect(201);
+});
+
+it("returns a 400 with an invalid email", async () => {
+    return request(app)
+        .post("/api/users/signup")
+        .send({
+            email: "invalidemail.com",
+            password: "test_password",
+        })
+        .expect(400);
+});
+
+it("returns a 400 with an invalid password", async () => {
+    return request(app)
+        .post("/api/users/signup")
+        .send({
+            email: "invalidemail.com",
+            password: "1",
+        })
+        .expect(400);
 });
