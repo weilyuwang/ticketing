42cbc3f373782cb192b47cf529c5bc04fae12c1c
weilyuwang <weilyuwang@gmail.com>
2020-06-10 20:48:14 -0400

    get rid of require auth middleware from current user route for now

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,17 +1,11 @@
 import express from "express";
 import { currentUserMiddleware } from "../middlewares/current-user";
-import { requireAuthMiddleware } from "../middlewares/require-auth";
 
 const router = express.Router();
 
-router.get(
-    "/api/users/currentuser",
-    currentUserMiddleware,
-    requireAuthMiddleware,
-    (req, res) => {
-        res.send({ currentUser: req.currentUser || null });
-        // send { currentUser: user_info_payload } or send { currentUser: null }
-    }
-);
+router.get("/api/users/currentuser", currentUserMiddleware, (req, res) => {
+    res.send({ currentUser: req.currentUser || null });
+    // send { currentUser: user_info_payload } or send { currentUser: null }
+});
 
 export { router as currentUserRouter };
