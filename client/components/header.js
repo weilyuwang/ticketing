28539066ac4fc674a7bbdcba2428d00d4e83edaa
weilyuwang <weilyuwang@gmail.com>
2020-06-14 16:36:50 -0400

    conditionally show links on the header

--- client/components/header.js
+++ client/components/header.js
@@ -1,6 +1,25 @@
 import Link from "next/link";
 
 export default ({ currentUser }) => {
+  // links = [false, false, { label: "Sign Out", href: "/auth/signout" }]
+  // or = [{ label: "Sign Up", href: "/auth/signup" }, { label: "Sign In", href: "/auth/signin" }, false]
+
+  const links = [
+    !currentUser && { label: "Sign Up", href: "/auth/signup" },
+    !currentUser && { label: "Sign In", href: "/auth/signin" },
+    currentUser && { label: "Sign Out", href: "/auth/signout" },
+  ]
+    .filter((linkConfig) => linkConfig)
+    .map(({ label, href }) => {
+      return (
+        <li key={href} className="nav-item">
+          <Link href={href}>
+            <a className="nav-link">{label}</a>
+          </Link>
+        </li>
+      );
+    });
+
   return (
     <nav className="navbar navbar-light bg-light">
       <Link href="/">
@@ -8,9 +27,7 @@ export default ({ currentUser }) => {
       </Link>
 
       <div className="d-flex justify-content-end">
-        <ul className="nav d-flex align-items-center">
-          {currentUser ? "Sign out" : "Sign in/up"}
-        </ul>
+        <ul className="nav d-flex align-items-center">{links}</ul>
       </div>
     </nav>
   );
