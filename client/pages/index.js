f29e3e207851e926ca70a8d01bffcba3603d1a6f
weilyuwang <weilyuwang@gmail.com>
2020-06-14 12:56:52 -0400

    fetch data during SSR

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,17 @@
-export default () => {
+import axios from "axios";
+
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
+
   return <h1>Landing page</h1>;
 };
+
+// executed during the SSR process
+LandingPage.getInitialProps = async () => {
+  // make the request on the server
+  const response = await axios.get("/api/users/currentUser");
+
+  return response.data;
+};
+
+export default LandingPage;
