89c5f99385d7cc6ff792d10b6cd56fcaac936457
weilyuwang <weilyuwang@gmail.com>
2020-06-14 15:12:36 -0400

    determine whether on the browser or server

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,17 +1,22 @@
 import axios from "axios";
 
 const LandingPage = ({ currentUser }) => {
-  console.log(currentUser);
+  // console.log(currentUser);
 
   return <h1>Landing page</h1>;
 };
 
 // executed during the SSR process
 LandingPage.getInitialProps = async () => {
-  // make the request on the server
-  const response = await axios.get("/api/users/currentUser");
+  if (typeof window === "undefined") {
+    // we are on the server!
+    // requests should be made to http://ingress-nginx....
+  } else {
+    // we are on the browser!
+    // requests can be made with a base url of ''
+  }
 
-  return response.data;
+  return {};
 };
 
 export default LandingPage;
