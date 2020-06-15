492e414f71ec634e56757ebf3ce7ff2a33c91838
weilyuwang <weilyuwang@gmail.com>
2020-06-15 13:19:23 -0400

    create first tickets service route

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -4,6 +4,8 @@ import { json } from "body-parser";
 import cookieSession from "cookie-session";
 import { errorHandlerMiddleware, NotFoundError } from "@wwticketing/common";
 
+import { createTicketRouter } from "./routes/new";
+
 const app = express();
 app.set("trust proxy", true); // trust ingress & nginx proxy
 app.use(json());
@@ -15,6 +17,7 @@ app.use(
 );
 
 // express routes
+app.use(createTicketRouter);
 
 // use express-async-errors lib behind the scene to handle async errors
 app.all("*", async (req, res) => {
--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,7 +1,11 @@
 import request from "supertest";
 import { app } from "../../app";
 
-it("has a route handler listening to /api/tickets for post request", async () => {});
+it("has a route handler listening to /api/tickets for post request", async () => {
+    const response = await request(app).post("/api/tickets").send({});
+
+    expect(response.status).not.toEqual(404);
+});
 
 it("can only be accessed if user is signed in", async () => {});
 
