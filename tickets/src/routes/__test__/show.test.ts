bfd07891e623d5971618be9f7c7f1deb9803c257
weilyuwang <weilyuwang@gmail.com>
2020-06-15 15:00:02 -0400

    create GET ticket by id test

---
+++ tickets/src/routes/__test__/show.test.ts
@@ -0,0 +1,27 @@
+import request from "supertest";
+import { app } from "../../app";
+
+it("returns a 404 if the ticket is not found", async () => {
+    await request(app).get("/api/tickets/must_not_exist_id").send().expect(404);
+});
+
+it("returns the ticket if the ticket is found", async () => {
+    const title = "concert";
+    const price = 50;
+
+    // sign in and create a ticket first
+    const response = await request(app)
+        .post("/api/tickets")
+        .set("Cookie", global.signin_and_get_cookie())
+        .send({ title, price })
+        .expect(201);
+
+    // then test if the ticket info can be found
+    const ticketResponse = await request(app)
+        .get(`/api/tickets/${response.body.id}`)
+        .send()
+        .expect(200);
+
+    expect(ticketResponse.body.title).toEqual(title);
+    expect(ticketResponse.body.price).toEqual(price);
+});
