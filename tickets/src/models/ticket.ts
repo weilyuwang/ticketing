6c9bdd11461a8310b7fd8650122b94995bd4a6ac
weilyuwang <weilyuwang@gmail.com>
2020-06-15 14:40:13 -0400

    define Ticket Model

--- tickets/src/models/ticket.ts
+++ tickets/src/models/ticket.ts
@@ -15,3 +15,39 @@ interface TicketDoc extends mongoose.Document {
 interface TicketModel extends mongoose.Model<TicketDoc> {
     build(attrs: TicketAttrs): TicketDoc;
 }
+
+const ticketSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String, // String type is used by mongoose, not typescript and hence the captical S
+            required: true,
+        },
+        price: {
+            type: Number,
+            required: true,
+        },
+    },
+    {
+        toJSON: {
+            transform(doc, ret) {
+                // modify ret: returned json object directly
+                // convert id => _id (normalize id property as other databases use _id)
+                ret.id = ret._id;
+                delete ret.id;
+            },
+        },
+    }
+);
+
+// The only reason we create this static build method is to let Typescript to check the
+// attribute type (attrs)
+ticketSchema.statics.build = (attrs: TicketAttrs) => {
+    return new Ticket(attrs);
+};
+
+const Ticket: TicketModel = mongoose.model<TicketDoc, TicketModel>(
+    "Ticket",
+    ticketSchema
+);
+
+export { Ticket };
