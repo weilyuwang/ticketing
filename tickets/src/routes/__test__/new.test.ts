10a6c729e0d8250e4e31488502e5dddef02d0b6d
weilyuwang <weilyuwang@gmail.com>
2020-06-15 13:53:49 -0400

    set up other tests

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -22,8 +22,53 @@ it("returns a status other than 401 if the user is signed in", async () => {
     expect(response.status).not.toEqual(401);
 });
 
-it("returns an error if an invalid title is provided", async () => {});
+it("returns an error if an invalid title is provided", async () => {
+    // case #1: title is invalid
+    await request(app)
+        .post("/api/tickets")
+        .set("Cookie", global.signin_and_get_cookie())
+        .send({
+            title: "",
+            price: 10,
+        })
+        .expect(400);
 
-it("returns an error if an invalid price is provided", async () => {});
+    // case #2: no title at all
+    await request(app)
+        .post("/api/tickets")
+        .set("Cookie", global.signin_and_get_cookie())
+        .send({
+            price: 10,
+        })
+        .expect(400);
+});
 
-it("creates a ticket with valid inputs", async () => {});
+it("returns an error if an invalid price is provided", async () => {
+    // case #1: price is invalid
+    await request(app)
+        .post("/api/tickets")
+        .set("Cookie", global.signin_and_get_cookie())
+        .send({
+            title: "Valid Title",
+            price: -10,
+        })
+        .expect(400);
+
+    // case #2: no price at all
+    await request(app)
+        .post("/api/tickets")
+        .set("Cookie", global.signin_and_get_cookie())
+        .send({
+            title: "Valid Title",
+        })
+        .expect(400);
+});
+
+it("creates a ticket with valid inputs", async () => {
+    // empty input
+    await request(app)
+        .post("/api/tickets")
+        .set("Cookie", global.signin_and_get_cookie())
+        .send({})
+        .expect(400);
+});
