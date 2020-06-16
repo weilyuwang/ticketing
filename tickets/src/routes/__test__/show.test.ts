fa0f58270665f66189045c503c49cee55c2993aa
weilyuwang <weilyuwang@gmail.com>
2020-06-15 21:45:08 -0400

    update common lib

--- tickets/src/routes/__test__/show.test.ts
+++ tickets/src/routes/__test__/show.test.ts
@@ -2,7 +2,10 @@ import request from "supertest";
 import { app } from "../../app";
 
 it("returns a 404 if the ticket is not found", async () => {
-    await request(app).get("/api/tickets/must_not_exist_id").send().expect(404);
+    await request(app)
+        .get("/api/tickets/must_not_exist_id") // id passed in must be a single String of 12 bytes or a string of 24 hex characters
+        .send()
+        .expect(404);
 });
 
 it("returns the ticket if the ticket is found", async () => {
