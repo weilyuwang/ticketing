e130a1f1ba14995f9f964aca04d5224ecb4ea50c
weilyuwang <weilyuwang@gmail.com>
2020-06-19 18:50:41 -0400

    graceful shut down nats connection

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -17,6 +17,15 @@ const start = async () => {
             "somerandomstring",
             "http://nats-srv:4222"
         );
+
+        // Gracefully shut down NATS connection
+        natsWrapper.client.on("close", () => {
+            console.log("NATS connection closed!");
+            process.exit();
+        });
+        process.on("SIGINT", () => natsWrapper.client.close());
+        process.on("SIGTERM", () => natsWrapper.client.close());
+
         await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
