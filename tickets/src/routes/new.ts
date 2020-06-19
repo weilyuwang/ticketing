6e0e85878dbb6bfed2b4ff2fcc823d013c154b77
weilyuwang <weilyuwang@gmail.com>
2020-06-19 18:34:50 -0400

    access nats client from other files

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -6,6 +6,7 @@ import {
 import { body } from "express-validator";
 import { Ticket } from "../models/ticket";
 import { TicketCreatedPublisher } from "../events/publishers/ticket-created-publisher";
+import { natsWrapper } from "../nats-wrapper";
 
 const router = express.Router();
 
@@ -32,7 +33,7 @@ router.post(
         await ticket.save();
 
         // publish ticket create event/message to NATS Streaming server
-        new TicketCreatedPublisher(client).publish({
+        new TicketCreatedPublisher(natsWrapper.client).publish({
             id: ticket.id,
             title: ticket.title,
             price: ticket.price,
