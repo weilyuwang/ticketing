cedf40cc0b2ab422a6b8b953e9f04b94fc3291de
weilyuwang <weilyuwang@gmail.com>
2020-06-22 16:21:50 -0400

    start to impl new order creation logic

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -2,9 +2,12 @@ import express, { Request, Response } from "express";
 import {
     requireAuthMiddleware,
     validateRequestMiddleware,
+    NotFoundError,
 } from "@wwticketing/common";
 import { body } from "express-validator";
 import mongoose from "mongoose";
+import { Ticket } from "../models/ticket";
+import { Order } from "../models/order";
 
 const router = express.Router();
 
@@ -20,6 +23,22 @@ router.post(
     ],
     validateRequestMiddleware,
     async (req: Request, res: Response) => {
+        const { ticketId } = req.body;
+
+        // Find the ticket the user is trying to order in the database
+        const ticket = await Ticket.findById(ticketId);
+        if (!ticket) {
+            throw new NotFoundError();
+        }
+
+        // Make sure that this ticket is not already reserved
+
+        // Calculate an expiration date for this order
+
+        // Build the order and save it to database
+
+        // Publish an event saying that an order was created
+
         res.send({});
     }
 );
