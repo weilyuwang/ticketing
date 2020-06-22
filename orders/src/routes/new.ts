d5acb5daf69e4e1ca784ac1f8db3e1be75555d79
weilyuwang <weilyuwang@gmail.com>
2020-06-22 15:15:09 -0400

    add middlewares to new route

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,9 +1,27 @@
 import express, { Request, Response } from "express";
+import {
+    requireAuthMiddleware,
+    validateRequestMiddleware,
+} from "@wwticketing/common";
+import { body } from "express-validator";
+import mongoose from "mongoose";
 
 const router = express.Router();
 
-router.post("/api/orders", async (req: Request, res: Response) => {
-    res.send({});
-});
+router.post(
+    "/api/orders",
+    requireAuthMiddleware,
+    [
+        body("ticketId")
+            .not()
+            .isEmpty()
+            .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+            .withMessage("TicketId must be provided"),
+    ],
+    validateRequestMiddleware,
+    async (req: Request, res: Response) => {
+        res.send({});
+    }
+);
 
 export { router as newOrderRouter };
