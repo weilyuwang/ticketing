38f5835761d6909abcb2f7109821ba00271ceb6a
weilyuwang <weilyuwang@gmail.com>
2020-06-22 16:02:05 -0400

    replace string with OrderStatus enum

--- orders/src/models/order.ts
+++ orders/src/models/order.ts
@@ -1,15 +1,16 @@
 import mongoose, { mongo } from "mongoose";
+import { OrderStatus } from "@wwticketing/common";
 
 interface OrderAttrs {
     userId: string;
-    status: string;
+    status: OrderStatus;
     expiresAt: Date;
     ticket: TicketDoc;
 }
 
 interface OrderDoc extends mongoose.Document {
     userId: string;
-    status: string;
+    status: OrderStatus;
     expiresAt: Date;
     ticket: TicketDoc;
 }
@@ -27,6 +28,8 @@ const orderSchema = new mongoose.Schema(
         status: {
             type: String,
             required: true,
+            enum: Object.values(OrderStatus),
+            default: OrderStatus.Created,
         },
         expiresAt: {
             type: mongoose.Schema.Types.Date,
