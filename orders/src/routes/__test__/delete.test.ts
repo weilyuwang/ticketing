5ccc92e1a2c2ca94c2c2541d064172dbb7559f7a
weilyuwang <weilyuwang@gmail.com>
2020-06-23 16:53:04 -0400

    impl and tests for delete handler

---
+++ orders/src/routes/__test__/delete.test.ts
@@ -0,0 +1,38 @@
+import request from "supertest";
+import { app } from "../../app";
+import { Ticket } from "../../models/ticket";
+import { Order, OrderStatus } from "../../models/order";
+
+it("marks an order as cancelled", async () => {
+    // Create a ticket
+    const ticket = Ticket.build({
+        title: "concert",
+        price: 20,
+    });
+    await ticket.save();
+
+    // Create a mock user
+    const user = global.signin_and_get_cookie();
+
+    // Make a request to create an order
+    const { body: order } = await request(app)
+        .post("/api/orders")
+        .set("Cookie", user)
+        .send({ ticketId: ticket.id })
+        .expect(201);
+
+    expect(order.status).not.toEqual(OrderStatus.Cancelled);
+
+    // Make a request to cancel the order
+    await request(app)
+        .delete(`/api/orders/${order.id}`)
+        .set("Cookie", user)
+        .send()
+        .expect(204);
+
+    // Make sure the order is actually cancelled
+    const updatedOrder = await Order.findById(order.id);
+    expect(updatedOrder?.status).toEqual(OrderStatus.Cancelled);
+});
+
+it("", async () => {});
--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,9 +1,38 @@
 import express, { Request, Response } from "express";
+import {
+    requireAuthMiddleware,
+    NotFoundError,
+    NotAuthorizedError,
+} from "@wwticketing/common";
+import { Order, OrderStatus } from "../models/order";
 
 const router = express.Router();
 
-router.delete("/api/orders/:orderId", async (req: Request, res: Response) => {
-    res.send({});
-});
+// We are only actually `deleting` an order, but instead set Order Status to `Cancelled`
+// `patch` HTTP Method might be more suitable for this case
+router.delete(
+    "/api/orders/:orderId",
+    requireAuthMiddleware,
+    async (req: Request, res: Response) => {
+        const { orderId } = req.params;
+
+        const order = await Order.findById(orderId);
+
+        if (!order) {
+            throw new NotFoundError();
+        }
+
+        if (order.userId !== req.currentUser!.id) {
+            throw new NotAuthorizedError();
+        }
+
+        order.status = OrderStatus.Cancelled;
+        await order.save();
+
+        // TODO: Publishing an event saying that the order is cancelled
+
+        res.status(204).send(order);
+    }
+);
 
 export { router as deleteOrderRouter };
