ff780eb43cef0a07f241c6b5c2c0340d4c91fdab
weilyuwang <weilyuwang@gmail.com>
2020-06-23 16:33:47 -0400

    show handler impl

--- orders/src/routes/show.ts
+++ orders/src/routes/show.ts
@@ -1,9 +1,30 @@
 import express, { Request, Response } from "express";
+import {
+    requireAuthMiddleware,
+    NotFoundError,
+    NotAuthorizedError,
+} from "@wwticketing/common";
+import { Order } from "../models/order";
 
 const router = express.Router();
 
-router.get("/api/orders/:orderId", async (req: Request, res: Response) => {
-    res.send({});
-});
+router.get(
+    "/api/orders/:orderId",
+    requireAuthMiddleware,
+    async (req: Request, res: Response) => {
+        const order = await Order.findById(req.params.orderId).populate(
+            "ticket"
+        );
+
+        if (!order) {
+            throw new NotFoundError();
+        }
+        if (order.userId !== req.currentUser!.id) {
+            throw new NotAuthorizedError();
+        }
+
+        res.send(order);
+    }
+);
 
 export { router as showOrderRouter };
