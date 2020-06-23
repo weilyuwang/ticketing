303fe75ebbda20383bd34a81684195c71fffa773
weilyuwang <weilyuwang@gmail.com>
2020-06-23 17:34:54 -0400

    publish order cancelled event

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -5,6 +5,8 @@ import {
     NotAuthorizedError,
 } from "@wwticketing/common";
 import { Order, OrderStatus } from "../models/order";
+import { natsWrapper } from "../nats-wrapper";
+import { OrderCancelledPublisher } from "../events/publishers/order-cancelled-publisher";
 
 const router = express.Router();
 
@@ -16,7 +18,7 @@ router.delete(
     async (req: Request, res: Response) => {
         const { orderId } = req.params;
 
-        const order = await Order.findById(orderId);
+        const order = await Order.findById(orderId).populate("ticket");
 
         if (!order) {
             throw new NotFoundError();
@@ -29,7 +31,13 @@ router.delete(
         order.status = OrderStatus.Cancelled;
         await order.save();
 
-        // TODO: Publishing an event saying that the order is cancelled
+        // Publishing an event saying that the order is cancelled
+        new OrderCancelledPublisher(natsWrapper.client).publish({
+            id: order.id,
+            ticket: {
+                id: order.ticket.id,
+            },
+        });
 
         res.status(204).send(order);
     }
