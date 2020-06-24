8fc890b4948005a5ed522ee7ab8c8cf0a4632868
weilyuwang <weilyuwang@gmail.com>
2020-06-24 14:39:34 -0400

    init listeners

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
 import { app } from "./app";
 import { natsWrapper } from "./nats-wrapper";
+import { TicketCreatedListener } from "./events/listeners/ticket-created-listener";
+import { TicketUpdatedListener } from "./events/listeners/ticket-updated-listener";
 
 const start = async () => {
     // First check if JWT_KEY is defined
@@ -19,6 +21,9 @@ const start = async () => {
     if (!process.env.NATS_CLIENT_ID) {
         throw new Error("NATS_CLIENT_ID must be defined");
     }
+
+    new TicketCreatedListener(natsWrapper.client).listen();
+    new TicketUpdatedListener(natsWrapper.client).listen();
 
     try {
         await natsWrapper.connect(
