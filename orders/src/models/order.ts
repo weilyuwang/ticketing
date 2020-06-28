4fb3fd4a0ac29ebe4fb88b5979dbfe12e2010bb5
weilyuwang <weilyuwang@gmail.com>
2020-06-28 16:14:51 -0400

    add version property to orders model

--- orders/src/models/order.ts
+++ orders/src/models/order.ts
@@ -1,6 +1,8 @@
-import mongoose, { mongo } from "mongoose";
+import mongoose from "mongoose";
 import { OrderStatus } from "@wwticketing/common";
 import { TicketDoc } from "./ticket";
+import { updateIfCurrentPlugin } from 'mongoose-update-if-current'
+
 
 export { OrderStatus };
 
@@ -16,6 +18,7 @@ interface OrderDoc extends mongoose.Document {
     status: OrderStatus;
     expiresAt: Date;
     ticket: TicketDoc;
+    version: number;
 }
 
 interface OrderModel extends mongoose.Model<OrderDoc> {
@@ -51,6 +54,9 @@ const orderSchema = new mongoose.Schema(
         },
     }
 );
+
+orderSchema.set('versionKey', 'version')
+orderSchema.plugin(updateIfCurrentPlugin)
 
 orderSchema.statics.build = (attrs: OrderAttrs) => {
     return new Order(attrs);
