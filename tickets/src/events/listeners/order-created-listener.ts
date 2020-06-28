d0b7f18edeb7170e1638176310fde6adff7a5b17
weilyuwang <weilyuwang@gmail.com>
2020-06-28 18:43:58 -0400

    update common module

--- tickets/src/events/listeners/order-created-listener.ts
+++ tickets/src/events/listeners/order-created-listener.ts
@@ -2,6 +2,8 @@ import { Listener, OrderCreatedEvent, Subjects } from '@wwticketing/common'
 import { Message } from 'node-nats-streaming';
 import { queueGroupName } from './queue-group-name'
 import { Ticket } from '../../models/ticket'
+import { TicketUpdatedPublisher } from '../publishers/ticket-updated-publisher'
+import { natsWrapper } from '../../nats-wrapper'
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   readonly subject = Subjects.OrderCreated;
