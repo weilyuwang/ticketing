c317b05090e2d37f4c546aae7a97602a98f09661
weilyuwang <weilyuwang@gmail.com>
2020-06-29 17:05:26 -0400

    queueing a job on OrderCreateEvent arrival

--- expiration/src/events/listeners/order-created-listener.ts
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -1,14 +1,18 @@
 import { Listener, OrderCreatedEvent, Subjects } from '@wwticketing/common'
 import { Message } from 'node-nats-streaming';
 import { queueGroupName } from './queue-group-name'
+import { expirationQueue } from '../../queues/expiration-queue'
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
 
   readonly subject: Subjects.OrderCreated = Subjects.OrderCreated;
   readonly queueGroupName: string = queueGroupName;
 
-  onMessage(data: OrderCreatedEvent['data'], msg: Message) {
+  async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
+    await expirationQueue.add({
+      orderId: data.id
+    })
 
+    msg.ack()
   }
-
 }
--- expiration/src/index.ts
+++ expiration/src/index.ts
@@ -1,4 +1,5 @@
 import { natsWrapper } from "./nats-wrapper";
+import { OrderCreatedListener } from './events/listeners/order-created-listener'
 
 const start = async () => {
 
@@ -26,6 +27,9 @@ const start = async () => {
         });
         process.on("SIGINT", () => natsWrapper.client.close());
         process.on("SIGTERM", () => natsWrapper.client.close());
+
+        // start listen to OrderCreatedEvent
+        new OrderCreatedListener(natsWrapper.client).listen()
 
     } catch (err) {
         console.log(err);
