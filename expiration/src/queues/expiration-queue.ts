8793c6488b4ca57bc6d3c3418160534d157b8664
weilyuwang <weilyuwang@gmail.com>
2020-06-29 16:59:34 -0400

    create a queue

---
+++ expiration/src/queues/expiration-queue.ts
@@ -0,0 +1,17 @@
+import Queue from 'bull'
+
+interface Payload {
+  orderId: string;
+}
+
+const expirationQueue = new Queue<Payload>('order:expiration', {
+  redis: {
+    host: process.env.REDIS_HOST
+  }
+})
+
+expirationQueue.process(async (job: Queue.Job<Payload>) => {
+  console.log('I want to publish an expiration:complete event for orderId', job.data.orderId)
+})
+
+export { expirationQueue }
