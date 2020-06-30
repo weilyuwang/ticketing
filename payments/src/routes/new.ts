ab27fd724df02f74e782642d0e567fc155568768
weilyuwang <weilyuwang@gmail.com>
2020-06-30 16:28:57 -0400

    use stripe api to charge

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -4,7 +4,7 @@ import {
   requireAuthMiddleware, validateRequestMiddleware, BadRequestError, NotFoundError, NotAuthorizedError, OrderStatus
 } from '@wwticketing/common'
 import { Order } from '../models/order'
-
+import { stripe } from '../stripe'
 
 const router = express.Router()
 
@@ -31,6 +31,12 @@ router.post('/api/payments',
     if (order.status === OrderStatus.Cancelled) {
       throw new BadRequestError('Cannot pay for an cancelled order')
     }
+
+    await stripe.charges.create({
+      currency: 'usd',
+      amount: order.price * 100, // convert to cents
+      source: token,
+    })
 
     res.send({ success: true })
   })
