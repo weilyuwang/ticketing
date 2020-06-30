6488675f07dbca140b436fafed70d62c29ee9f00
weilyuwang <weilyuwang@gmail.com>
2020-06-29 21:24:13 -0400

    payments service init setup

---
+++ payments/src/app.ts
@@ -0,0 +1,37 @@
+import express from "express";
+import "express-async-errors";
+import { json } from "body-parser";
+import cookieSession from "cookie-session";
+import {
+    errorHandlerMiddleware,
+    NotFoundError,
+    currentUserMiddleware,
+} from "@wwticketing/common";
+
+
+const app = express();
+app.set("trust proxy", true); // trust ingress & nginx proxy
+app.use(json());
+
+// use cookieSession to add session property to req object
+app.use(
+    cookieSession({
+        signed: false, // disable encryption on the cookie - JWT is already secured
+        secure: process.env.NODE_ENV !== "test", // *** HTTPS connection only ***, but exception made for testing
+    })
+);
+
+// middleware to add currentUser to req
+app.use(currentUserMiddleware);
+
+// express routes
+
+// use express-async-errors lib behind the scene to handle async errors
+app.all("*", async (req, res) => {
+    throw new NotFoundError();
+});
+
+// middleware to handle all kinds of errors
+app.use(errorHandlerMiddleware);
+
+export { app };
