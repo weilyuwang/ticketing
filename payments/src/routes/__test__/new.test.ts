f5a86ac3273049e6c6b370eadee6f0ec740bd205
weilyuwang <weilyuwang@gmail.com>
2020-06-30 12:16:52 -0400

    add errors handling and impl tests for new payment route

---
+++ payments/src/routes/__test__/new.test.ts
@@ -0,0 +1,38 @@
+import { app } from '../../app'
+import request from 'supertest'
+import mongoose from 'mongoose'
+import { OrderStatus } from '@wwticketing/common'
+import { Order } from '../../models/order'
+
+it('returns a 404 when purchasing an order that does not exist', async () => {
+  await request(app).post('/api/payments')
+    .set('Cookie', global.signin_and_get_cookie())
+    .send({
+      token: 'aaaaaaa',
+      orderId: mongoose.Types.ObjectId().toHexString()
+    })
+    .expect(404)
+})
+
+it('returns a 401 when purchasing an order that does not belong to the current user', async () => {
+  const order = Order.build({
+    id: mongoose.Types.ObjectId().toHexString(),
+    userId: mongoose.Types.ObjectId().toHexString(),
+    version: 0,
+    price: 100,
+    status: OrderStatus.Created,
+  })
+  await order.save()
+
+  await request(app).post('/api/payments')
+    .set('Cookie', global.signin_and_get_cookie())
+    .send({
+      token: 'aaaaaaa',
+      orderId: order.id
+    })
+    .expect(401)
+})
+
+it('returns a 400 when purchasing a cancelled order', async () => {
+
+})
--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from 'express'
 import { body } from 'express-validator'
 import {
-  requireAuthMiddleware, validateRequestMiddleware, BadRequestError, NotFoundError
+  requireAuthMiddleware, validateRequestMiddleware, BadRequestError, NotFoundError, NotAuthorizedError, OrderStatus
 } from '@wwticketing/common'
 import { Order } from '../models/order'
 
@@ -16,6 +16,22 @@ router.post('/api/payments',
   ],
   validateRequestMiddleware,
   async (req: Request, res: Response) => {
+
+    const { token, orderId } = req.body
+
+    const order = await Order.findById(orderId)
+    if (!order) {
+      throw new NotFoundError()
+    }
+
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError()
+    }
+
+    if (order.status === OrderStatus.Cancelled) {
+      throw new BadRequestError('Cannot pay for an cancelled order')
+    }
+
     res.send({ success: true })
   })
 
