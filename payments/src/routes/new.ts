d1520d225c78bfc2a24f912cedb3cb4c60829f00
weilyuwang <weilyuwang@gmail.com>
2020-06-30 17:19:57 -0400

    add payment model

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -4,6 +4,7 @@ import {
   requireAuthMiddleware, validateRequestMiddleware, BadRequestError, NotFoundError, NotAuthorizedError, OrderStatus
 } from '@wwticketing/common'
 import { Order } from '../models/order'
+import { Payment } from '../models/payment'
 import { stripe } from '../stripe'
 
 const router = express.Router()
@@ -32,11 +33,17 @@ router.post('/api/payments',
       throw new BadRequestError('Cannot pay for an cancelled order')
     }
 
-    await stripe.charges.create({
+    const charge = await stripe.charges.create({
       currency: 'usd',
       amount: order.price * 100, // convert to cents
       source: token,
     })
+
+    const payment = Payment.build({
+      orderId: orderId,
+      stripeId: charge.id
+    })
+    await payment.save()
 
     res.status(201).send({ success: true })
   })
