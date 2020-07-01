cd68b05858a0d877263952222bdf6a8ee6c0b15f
weilyuwang <weilyuwang@gmail.com>
2020-07-01 16:14:13 -0400

    save order complete status

--- orders/src/events/listeners/payment-created-listener.ts
+++ orders/src/events/listeners/payment-created-listener.ts
@@ -1,25 +1,29 @@
-import { Subjects, Listener, PaymentCreatedEvent, OrderStatus } from '@wwticketing/common'
-import { Message } from 'node-nats-streaming';
-import { queueGroupName } from './queue-group-name';
-import { Order } from '../../models/order'
-
+import {
+    Subjects,
+    Listener,
+    PaymentCreatedEvent,
+    OrderStatus,
+} from "@wwticketing/common";
+import { Message } from "node-nats-streaming";
+import { queueGroupName } from "./queue-group-name";
+import { Order } from "../../models/order";
 
 export class PaymentCreatedListener extends Listener<PaymentCreatedEvent> {
-  readonly subject = Subjects.PaymentCreated;
-  readonly queueGroupName: string = queueGroupName;
+    readonly subject = Subjects.PaymentCreated;
+    readonly queueGroupName: string = queueGroupName;
 
-  async onMessage(data: PaymentCreatedEvent['data'], msg: Message) {
-    const order = await Order.findById(data.orderId)
+    async onMessage(data: PaymentCreatedEvent["data"], msg: Message) {
+        const order = await Order.findById(data.orderId);
 
-    if (!order) {
-      throw new Error('Order not found')
+        if (!order) {
+            throw new Error("Order not found");
+        }
+
+        order.set({
+            status: OrderStatus.Complete,
+        });
+        await order.save();
+
+        msg.ack();
     }
-
-    order.set({
-      status: OrderStatus.Complete
-    })
-
-    msg.ack()
-
-  }
 }
