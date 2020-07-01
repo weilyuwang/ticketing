70c94688778729f37778a8a85e381ca648a039bf
weilyuwang <weilyuwang@gmail.com>
2020-07-01 17:50:18 -0400

    startup message to orders service

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -3,10 +3,12 @@ import { app } from "./app";
 import { natsWrapper } from "./nats-wrapper";
 import { TicketCreatedListener } from "./events/listeners/ticket-created-listener";
 import { TicketUpdatedListener } from "./events/listeners/ticket-updated-listener";
-import { ExpirationCompleteListener } from './events/listeners/expiration-complete-listener'
-import { PaymentCreatedListener } from './events/listeners/payment-created-listener'
+import { ExpirationCompleteListener } from "./events/listeners/expiration-complete-listener";
+import { PaymentCreatedListener } from "./events/listeners/payment-created-listener";
 
 const start = async () => {
+    console.log("Starting Orders Service...");
+
     // First check if JWT_KEY is defined
     if (!process.env.JWT_KEY) {
         throw new Error("JWT_KEY must be defined");
@@ -41,8 +43,8 @@ const start = async () => {
 
         new TicketCreatedListener(natsWrapper.client).listen();
         new TicketUpdatedListener(natsWrapper.client).listen();
-        new ExpirationCompleteListener(natsWrapper.client).listen()
-        new PaymentCreatedListener(natsWrapper.client).listen()
+        new ExpirationCompleteListener(natsWrapper.client).listen();
+        new PaymentCreatedListener(natsWrapper.client).listen();
 
         await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
