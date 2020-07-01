ac65d7b2906b1c274d58cdae393860cc9ab73a6c
weilyuwang <weilyuwang@gmail.com>
2020-06-30 22:16:14 -0400

    handle ticket creation

--- client/pages/tickets/new.js
+++ client/pages/tickets/new.js
@@ -1,8 +1,18 @@
 import { useState } from "react";
+import useRequest from "../../hooks/use-request";
 
 const NewTicket = () => {
   const [price, setPrice] = useState("");
   const [title, setTitle] = useState("");
+  const { doRequest, errors } = useRequest({
+    url: "/api/tickets",
+    method: "post",
+    body: {
+      title,
+      price,
+    },
+    onSuccess: (ticket) => console.log(ticket),
+  });
 
   console.log(typeof price);
 
@@ -14,10 +24,17 @@ const NewTicket = () => {
     setPrice(value.toFixed(2)); // returns a string representing a number in fixed-point notation.
   };
 
+  const onSubmit = (event) => {
+    event.preventDefault();
+
+    // make the request to POST /api/tickets
+    doRequest();
+  };
+
   return (
     <div>
       <h1>Create a Ticket</h1>
-      <form>
+      <form onSubmit={onSubmit}>
         <div className="form-group">
           <label>Title</label>
           <input
@@ -35,6 +52,7 @@ const NewTicket = () => {
             className="form-control"
           />
         </div>
+        {errors}
         <button className="btn btn-primary">Submit</button>
       </form>
     </div>
