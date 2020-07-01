dd90657a161fe5eb722385bc1df826cfae277811
weilyuwang <weilyuwang@gmail.com>
2020-06-30 22:25:45 -0400

    list all tickets on home page

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,14 +1,34 @@
-const LandingPage = ({ currentUser }) => {
-  return currentUser ? (
-    <h1> You are signed in </h1>
-  ) : (
-    <h1> You are not signed in </h1>
+const LandingPage = ({ currentUser, tickets }) => {
+  const ticketList = tickets.map((ticket) => {
+    return (
+      <tr key={ticket.id}>
+        <td>{ticket.title}</td>
+        <td>{ticket.price}</td>
+      </tr>
+    );
+  });
+
+  return (
+    <div>
+      <h1>Tickets</h1>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>{ticketList}</tbody>
+      </table>
+    </div>
   );
 };
 
 // executed during the SSR process
 LandingPage.getInitialProps = async (context, client, currentUser) => {
-  return {};
+  const { data } = await client.get("/api/tickets");
+
+  return { tickets: data };
 };
 
 export default LandingPage;
