978c9f5d3d177dfd77419b5e97f570bbb2fd3416
weilyuwang <weilyuwang@gmail.com>
2020-06-30 22:07:29 -0400

    handle inputs

--- client/pages/tickets/new.js
+++ client/pages/tickets/new.js
@@ -1,15 +1,39 @@
+import { useState } from "react";
+
 const NewTicket = () => {
+  const [price, setPrice] = useState("");
+  const [title, setTitle] = useState("");
+
+  console.log(typeof price);
+
+  const onBlur = () => {
+    const value = parseFloat(price);
+    if (isNaN(value)) {
+      return;
+    }
+    setPrice(value.toFixed(2)); // returns a string representing a number in fixed-point notation.
+  };
+
   return (
     <div>
       <h1>Create a Ticket</h1>
       <form>
         <div className="form-group">
           <label>Title</label>
-          <input className="form-control" />
+          <input
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            className="form-control"
+          />
         </div>
         <div className="form-group">
-          <label>Title</label>
-          <input className="form-control" />
+          <label>Price</label>
+          <input
+            value={price}
+            onBlur={onBlur}
+            onChange={(e) => setPrice(e.target.value)}
+            className="form-control"
+          />
         </div>
         <button className="btn btn-primary">Submit</button>
       </form>
