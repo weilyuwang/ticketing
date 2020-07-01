ea80cf8e62f53d1299f65154debeca970031720f
weilyuwang <weilyuwang@gmail.com>
2020-06-30 20:19:24 -0400

    listen for payment created event from orders service

---
+++ orders/src/events/listeners/payment-created-listener.ts
@@ -0,0 +1,25 @@
+import { Subjects, Listener, PaymentCreatedEvent, OrderStatus } from '@wwticketing/common'
+import { Message } from 'node-nats-streaming';
+import { queueGroupName } from './queue-group-name';
+import { Order } from '../../models/order'
+
+
+export class PaymentCreatedListener extends Listener<PaymentCreatedEvent> {
+  readonly subject = Subjects.PaymentCreated;
+  readonly queueGroupName: string = queueGroupName;
+
+  async onMessage(data: PaymentCreatedEvent['data'], msg: Message) {
+    const order = await Order.findById(data.orderId)
+
+    if (!order) {
+      throw new Error('Order not found')
+    }
+
+    order.set({
+      status: OrderStatus.Complete
+    })
+
+    msg.ack()
+
+  }
+}
--- orders/src/index.ts
+++ orders/src/index.ts
@@ -4,7 +4,7 @@ import { natsWrapper } from "./nats-wrapper";
 import { TicketCreatedListener } from "./events/listeners/ticket-created-listener";
 import { TicketUpdatedListener } from "./events/listeners/ticket-updated-listener";
 import { ExpirationCompleteListener } from './events/listeners/expiration-complete-listener'
-
+import { PaymentCreatedListener } from './events/listeners/payment-created-listener'
 
 const start = async () => {
     // First check if JWT_KEY is defined
@@ -42,6 +42,7 @@ const start = async () => {
         new TicketCreatedListener(natsWrapper.client).listen();
         new TicketUpdatedListener(natsWrapper.client).listen();
         new ExpirationCompleteListener(natsWrapper.client).listen()
+        new PaymentCreatedListener(natsWrapper.client).listen()
 
         await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
